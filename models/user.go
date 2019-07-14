058bd43dd010acb9cd33db876fc3b88ed11a040e
Jessica Paz <jessicamorim.42@gmail.com>
2019-07-14 16:05:48 -0300

    models: add a interface to describes all methods in user model

--- models/user.go
+++ models/user.go
@@ -1,35 +1,66 @@
 package models
 
 import (
-	"github.com/jessicapaz/desafio-stone/config"
+	"database/sql"
+	"errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
 // User model
 type User struct {
+	ID       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-// Create creates a user on database
-func (user *User) Create() error {
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	user.Password = string(hashedPassword)
-	stmt := "INSERT INTO users (email, password) VALUES ($1, $2)"
-	_, err := config.GetDB().Query(stmt, user.Email, user.Password)
-	if err != nil {
-		return err
+// UserModelImpl describes all methods of a UserModelImpl
+type UserModelImpl interface {
+	Create(user *User) (User, error)
+	ByEmail(email string) (User, error)
+}
+
+type UserModel struct {
+	db *sql.DB
+}
+
+// NewUserModel creates a new UserModel
+func NewUserModel(db *sql.DB) *UserModel {
+	return &UserModel{
+		db: db,
+	}
+}
+
+// Validate validates user data
+func (user *User) Validate() error {
+	if user.Password == "" {
+		return errors.New("password must not be empty")
+	}
+	if user.Email == "" {
+		return errors.New("email must not be empty")
 	}
 	return nil
 }
 
+// Create creates a user on database
+func (u *UserModel) Create(user *User) (User, error) {
+	newUser := User{}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	user.Password = string(hashedPassword)
+	stmt := "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *;"
+	result := u.db.QueryRow(stmt, user.Email, user.Password)
+	if err := result.Scan(&newUser.ID, &newUser.Email, &newUser.Password); err != nil {
+		return newUser, err
+	}
+	return newUser, nil
+}
+
 // ByEmail gets user by email
-func (user *User) ByEmail() (*User, error) {
-	u := new(User)
+func (u *UserModel) ByEmail(email string) (User, error) {
+	user := User{}
 	stmt := "SELECT email, password FROM users WHERE email=$1"
-	result := config.GetDB().QueryRow(stmt, user.Email)
-	if err := result.Scan(&u.Email, &u.Password); err != nil {
+	result := u.db.QueryRow(stmt, email)
+	if err := result.Scan(&user.Email, &user.Password); err != nil {
 		return user, err
 	}
-	return u, nil
+	return user, nil
 }
